985142ecfd8d75da0fa28b85649091ce666d623a
Htet Arkar Zaw <htetarkarzaw@creative-webstudio.com>
2019-11-10 16:35:23 +0630

    Initial Commit

---
+++ MVVMExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMExercise
+//
+//  Created by Htet Arkar Zaw on 10/11/2019.
+//  Copyright © 2019 Htet Arkar Zaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
